5725cb07585c63c656466e52b5c9eecc16335d26
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-04-24 20:58:16 +0300

    upadate task 1 : color switch

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+console.log(body);
+
+const startButton = document.querySelector('[data-start]');
+console.log(startButton);
+
+const stopButton = document.querySelector('[data-stop]');
+console.log(stopButton);
+
+const changeIntervalInMilliseconds = 1000;
+
+let lastBodyBgColor = null;
+
+function changeBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  lastBodyBgColor = body.style.backgroundColor;
+}
+
+startButton.addEventListener('click', ev => {
+  timerId = setInterval(changeBgColor, changeIntervalInMilliseconds);
+  startButton.disabled = true;
+  //   console.log(timerId);
+});
+
+stopButton.addEventListener('click', ev => {
+  body.style.backgroundColor = lastBodyBgColor;
+  //   console.log(lastBodyBgColor);
+  clearInterval(timerId);
+  startButton.disabled = false;
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,20 @@
+// Descris în documentație
+import flatpickr from 'flatpickr';
+// Import suplimentar de stil
+import 'flatpickr/dist/flatpickr.min.css';
+
+const input = document.getElementById('datetime-picker');
+
+const timer = document.getElementsByClassName('timer');
+
+const days = document.querySelector('[data-days]');
+console.log(days);
+
+const hours = document.querySelector('[data-hours]');
+console.log(hours);
+
+const minutes = document.querySelector('[data-minutes]');
+console.log(minutes);
+
+const seconds = document.querySelector('[data-seconds]');
+console.log(seconds);
