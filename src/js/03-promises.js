0f90117e8ea98fb745c5b4b0db8b2d1b8d57f8a1
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-05-08 11:58:06 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,43 +8,43 @@ const amount = form.querySelector("input[name = 'amount']");
 
 const createPromiseButton = form.querySelector('button');
 
-import Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
 
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         // Fulfill
-//         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//       } else {
-//         // Reject
-//         reject(`❌ Rejected promise ${position} in ${delay}ms`);
-//       }
-//     }, delayInp.value);
-//   });
-// }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delayInp.value);
+  });
+}
 
 createPromiseButton.addEventListener('click', ev => {
   ev.preventDefault();
-  console.log(delayInp);
+  // console.log(delayInp.value);
 
-  console.log(step);
+  // console.log(step.value);
 
-  console.log(amount);
+  // console.log(amount.value);
 
-  // for (let i = 0; i < amount.value; i++) {
-  //   setTimeout(() => {
-  //     createPromise(i, step.value);
-  //     // .then(({ position, delay }) => {
-  //     //   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  //     // })
-  //     // .catch(({ position, delay }) => {
-  //     //   console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  //     // });
-  //     console.log(`✅ Fulfilled promise ${i} in ${step.value}ms`);
-  //   }, step.value);
-  // }
+  for (let i = 1; i <= amount.value; i++) {
+    setTimeout(() => {
+      createPromise(i, step.value)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${i} in ${step.value}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${i} in ${step.value}ms`);
+        });
+      // console.log(`✅ Fulfilled promise ${i} in ${step.value}ms`);
+    }, step.value);
+  }
 });
 
 // createPromise(2, 1500)
