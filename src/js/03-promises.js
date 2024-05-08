3b7e487f7234ccc9a4eb7bf2054b5cb76af348e2
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-05-08 11:10:57 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,29 @@
 const form = document.querySelector('.form');
-console.log(form);
 
 const delayInp = form.querySelector("input[name = 'delay']");
-console.log(delayInp);
-console.log(delayInp.value);
 
 const step = form.querySelector("input[name = 'step']");
-console.log(step);
-// console.log(step.value);
 
 const amount = form.querySelector("input[name = 'amount']");
-console.log(amount);
-// console.log(amount.value);
 
 const createPromiseButton = form.querySelector('button');
-console.log(createPromissesButton);
 
 import Notiflix from 'notiflix';
 
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        // Fulfill
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else {
-        // Reject
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      }
-    }, delayInp.value);
-  });
-}
-// console.log(createPromise);
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         // Fulfill
+//         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       } else {
+//         // Reject
+//         reject(`❌ Rejected promise ${position} in ${delay}ms`);
+//       }
+//     }, delayInp.value);
+//   });
+// }
 
 createPromiseButton.addEventListener('click', ev => {
   ev.preventDefault();
